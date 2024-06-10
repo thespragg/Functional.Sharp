8c99a936d992cfbe458811dcda98667702551d81
Alistair Spragg <alistair.spragg@gmail.com>
2024-06-10 17:35:53 +0100

    Add helper functions (#2)

---
+++ Functional.Sharp/Extensions/ResultExtensions.cs
@@ -0,0 +1,50 @@
+using Functional.Sharp.Errors;
+using Functional.Sharp.Monads;
+
+namespace Functional.Sharp.Extensions;
+
+public static class ResultExtensions
+{
+    public static async Task<Result<TNext>> MapAsync<TValue, TNext>(
+        this Task<Result<TValue>> task,
+        Func<TValue, Task<TNext>> next)
+    {
+        var result = await task;
+        ArgumentNullException.ThrowIfNull(result);
+        ArgumentNullException.ThrowIfNull(next);
+
+        return await result.MatchAsync<Result<TNext>>(
+            async value => await next(value),
+            error => error
+        );
+    }
+
+    public static async Task<Result<TNext>> MatchAsync<TValue, TNext>(
+        this Task<Result<TValue>> task,
+        Func<TValue, Task<TNext>> success,
+        Func<Error, TNext> failure)
+    {
+        var result = await task;
+
+        return await result.MatchAsync<Result<TNext>>(
+            async value => await success(value),
+            error => failure(error)
+        );
+    }
+    
+    public static async Task<Result<TValue>> OnSuccessAsync<TValue>(
+        this Task<Result<TValue>> task,
+        Func<TValue, Task> onSuccess)
+    {
+        var result = await task;
+        return await result.OnSuccessAsync(onSuccess);
+    }
+    
+    public static async Task<Result<TValue>> OnFailureAsync<TValue>(
+        this Task<Result<TValue>> task,
+        Func<Error, Task> onFailure)
+    {
+        var result = await task;
+        return await result.OnFailureAsync(onFailure);
+    }
+}
